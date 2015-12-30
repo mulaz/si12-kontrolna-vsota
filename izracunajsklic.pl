c27fbbc512e0a1a44c96f419d8ef2535efb67b00
mulaz <dmulac@gmail.com>
2015-12-30 02:09:18 +0100

    added script

---
+++ izracunajsklic.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+use Data::Dumper;
+
+
+$inputnum=$ARGV[0] || die "no input number";
+
+print "number: $inputnum\n";
+
+$inputnum=~s/[^\d]//g;
+
+print "numeric-only number: $inputnum\n";
+
+@nums=split("",$inputnum);
+print "num array:\n";
+print Dumper(@nums);
+
+$ponder=@nums+1;
+
+print "starting ponder: $ponder\n";
+
+$sum=0;
+
+foreach $num (@nums) {
+    print "multiplying num $num with $ponder and sum is ";
+    $sum+=$num*$ponder;
+    $ponder--;
+    print "$sum \n";
+
+}
+
+print "final sum: $sum\n";
+
+$remainder=$sum % 11;
+
+print "sum % 11 = $remainder\n";
+
+if($remainder==10) {
+    $checksum=0; }
+elsif ($remainder == 0) {
+    print "warning remainder is divisible by 11\n";
+    $checksum=$0;
+}
+else {
+    $checksum=11-$remainder;
+}
+
+print "checksum: $checksum\n";
+
+print "number: ".$inputnum.$checksum."\n";
